1291862154938133e899cf6609cab8f63f3bd106
Tim White <fignewtonfood@gmail.com>
2015-08-13 10:36:22 -0700

    add to method to pass second spec

--- src/ScrabbleScore.php
+++ src/ScrabbleScore.php
@@ -3,8 +3,18 @@ class ScrabbleScore
 {
     function scrabbleScoreCalculator($word)
     {
-        if ($word == 'a') {
+        $one_point_array = array('a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't');
+        $two_point_array = array('d', 'g');
+        $three_point_array = array('b', 'c', 'm', 'p');
+        $four_point_array = array('f', 'h', 'v', 'w', 'y');
+        $five_point_array = array('k');
+        $eight_point_array = array('j', 'k');
+        $ten_point_array = array('q', 'z');
+
+        if (in_array($word, $one_point_array)) {
             return 1;
+        } else {
+            return 2;
         }
     }
 }
